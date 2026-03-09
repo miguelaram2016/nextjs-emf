67eff91921b9b54d03060033fa68c713eda4123c
Miguel Ramirez <miguelcodes7@gmail.com>
2026-03-08 20:32:48 -0500

    Use NEXT_ env vars, add .env.local for local dev

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -43,8 +43,8 @@ const ContactForm = () => {
 
     // EmailJS promise
     const emailPromise = emailjs.send(
-      process.env.VITE_APP_EMAILJS_SERVICE_ID || process.env.NEXT_EMAILJS_SERVICE_ID,
-      process.env.VITE_APP_EMAILJS_TEMPLATE_ID || process.env.NEXT_EMAILJS_TEMPLATE_ID,
+      process.env.NEXT_EMAILJS_SERVICE_ID,
+      process.env.NEXT_EMAILJS_TEMPLATE_ID,
       {
         from_name: form.name,
         to_name: "Miguel Ramirez",
@@ -52,7 +52,7 @@ const ContactForm = () => {
         to_email: "mramirez@elmarfitness.com",
         message: form.message + " /n " + form.email,
       },
-      process.env.VITE_APP_EMAILJS_PUBLIC_KEY || process.env.NEXT_EMAILJS_PUBLIC_KEY
+      process.env.NEXT_EMAILJS_PUBLIC_KEY
     );
 
     try {
